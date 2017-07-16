6d82e64594b0c98a8a36eeb9cbede6fd3b391dda
Tino Sambora <tsambora@Tinos-MacBook-Air.local>
2017-07-16 22:51:22 +0700

    add unvalidated transaction request

--- dummy-pos/js/App.js
+++ dummy-pos/js/App.js
@@ -3,7 +3,6 @@ import { Match } from 'react-router'
 import { Provider } from 'react-redux'
 import AppContainer from 'grommet/components/App'
 
-import store from './store'
 import Header from './Header'
 import Footer from './Footer'
 import Home from './Home'
@@ -11,15 +10,13 @@ import Home from './Home'
 const App = React.createClass({
     render() {
         return (
-            <Provider store={store}>
-                <AppContainer
-                    centered
-                    inline>
-                    <Header />
-                    <Match exactly pattern='/' component={Home} />
-                    <Footer />
-                </AppContainer>
-            </Provider>
+            <AppContainer
+                centered
+                inline>
+                <Header />
+                <Match exactly pattern='/' component={Home} />
+                <Footer />
+            </AppContainer>
         )
     }
 })
---
+++ dummy-pos/js/appConfig.js
@@ -0,0 +1,5 @@
+export default {
+    baseApiUrl: 'http://128.199.121.42:3000',
+    buyerId: '0x8629163eaf51b5166ccf1b74dd2d27c68f720662',
+    merchantId: '0x0629fb1714fe5017c7c29e206aa0283dcdb9d902'
+}
