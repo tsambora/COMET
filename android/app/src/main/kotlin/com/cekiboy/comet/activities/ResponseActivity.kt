f8d2895829b64b4d728a9668f63be4d7dc5f08cf
Irvan Adhitya <itockz@gmail.com>
2017-07-18 20:27:16 +0700

    [android] implement qr code generator

--- android/app/src/main/kotlin/com/cekiboy/comet/activities/ResponseActivity.kt
+++ android/app/src/main/kotlin/com/cekiboy/comet/activities/ResponseActivity.kt
@@ -1,11 +1,18 @@
 package com.cekiboy.comet.activities
 
+import android.graphics.Bitmap
 import android.os.Bundle
 import android.support.v4.app.NavUtils
+import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
+import android.util.DisplayMetrics
 import android.view.MenuItem
+import android.widget.ImageView
+import android.widget.TextView
 import com.cekiboy.comet.R
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.MultiFormatWriter
 
 /**
  * Created by itock on 7/18/2017.
@@ -17,16 +24,30 @@ class ResponseActivity : AppCompatActivity() {
     }
 
     private var toolbar: Toolbar? = null
+    private var imageViewQr: ImageView? = null
+    private var textViewResponse: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_response)
 
         toolbar = findViewById(R.id.toolbar) as Toolbar
+        imageViewQr = findViewById(R.id.qr) as ImageView
+        textViewResponse = findViewById(R.id.response) as TextView
 
         setSupportActionBar(toolbar)
         supportActionBar?.title = null
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val token = intent.getStringExtra(EXTRA_TRANSACTION_TOKEN)
+
+        if (!token.isNullOrEmpty()) {
+            val response = generateTokenResponse(token)
+            val bitmap = generateQr(response)
+
+            imageViewQr?.setImageBitmap(bitmap)
+            textViewResponse?.text = response
+        }
     }
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
@@ -38,5 +59,43 @@ class ResponseActivity : AppCompatActivity() {
             }
             else -> return super.onOptionsItemSelected(item)
         }
+    }
+
+    private fun generateTokenResponse(token: String): String {
+        // TODO: generate token response
+        return token
+    }
+
+    private fun generateQr(text: String): Bitmap {
+        val displayMetrics = DisplayMetrics()
+        windowManager.defaultDisplay.getMetrics(displayMetrics)
+        val displayWidth = displayMetrics.widthPixels
+        val displayHeight = displayMetrics.heightPixels
+
+        val dimension = (Math.min(displayWidth, displayHeight) * .8).toInt()
+
+        val writer = MultiFormatWriter()
+        val result = writer.encode(text, BarcodeFormat.QR_CODE, dimension, dimension, null)
+
+        val width = result.width
+        val height = result.height
+
+        val blockColor = ContextCompat.getColor(this, R.color.colorPrimaryDark)
+        val spaceColor = ContextCompat.getColor(this, android.R.color.transparent)
+
+        val pixels = IntArray(width * height)
+
+        repeat(height, { y ->
+            val offset = y * width
+
+            repeat(width, { x ->
+                pixels[offset + x] = if (result[x, y]) blockColor else spaceColor
+            })
+        })
+
+        val bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+        bitmap.setPixels(pixels, 0, width, 0, 0, width, height)
+
+        return bitmap
     }
 }
