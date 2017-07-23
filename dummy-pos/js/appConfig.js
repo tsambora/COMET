5db617bef7183233becfbdfe605ab4cf50874e93
ananta pandu <anpandumail@gmail.com>
2017-07-23 21:03:12 +0700

    [pos-frontend] fix env var

--- dummy-pos/js/appConfig.js
+++ dummy-pos/js/appConfig.js
@@ -1,4 +1,4 @@
 export default {
-    baseApiUrl: process.env.BASE_API_URL || 'http://localhost:3000',
+    baseApiUrl: process.env.BASE_API_URL || 'http://139.59.120.226:3000',
     merchantId: '0x7e5f4552091a69125d5dfcb7b8c2659029395bdf'
 }
