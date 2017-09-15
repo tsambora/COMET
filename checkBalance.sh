ff3d5dea7eeeaad2a7e2ebfe126b8183a15b8cce
Tino Sambora <tsambora@Tinos-MacBook-Air.local>
2017-09-15 10:44:50 +0100

    add checkbalance.sh

---
+++ checkBalance.sh
@@ -0,0 +1,7 @@
+echo "Loading buyer's balance..."
+curl http://139.59.120.226:3000/accounts/0x2b5ad5c4795c026514f8317c7a215e218dccd6cf/balance
+echo "\n"
+
+echo "Loading merchant's balance..."
+curl http://139.59.120.226:3000/accounts/0x7e5f4552091a69125d5dfcb7b8c2659029395bdf/balance
+echo "\n"
